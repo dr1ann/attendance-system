f252eb531c46707315bc0a4e083b0c7e41096e74
James <jamesdenoy56@gmail.com>
2024-04-22 21:09:07 +0800

    adjusted logo size on log in forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+// In your Tailwind CSS configuration file (tailwind.config.js)
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.{html,js}"],
@@ -8,8 +9,10 @@ module.exports = {
         smallerscreens: "430px",
         tabletscreen: "824px",
         bigscreens: '1920px'
-        
       },
+      textShadow: {
+        custom: '2px 4px 3px rgba(0,0,0,.3)'
+      }
     },
   },
   plugins: [],
