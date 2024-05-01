e1ae1b374b8116e72be9eefe5e88edc0931e70d7
James <jamesdenoy56@gmail.com>
2024-05-01 21:10:06 +0800

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,8 @@ module.exports = {
       },
       fontFamily: {
         'sans': ['Söhne', 'ui-sans-serif', 'system-ui', '-apple-system', 'Segoe UI', 'Roboto', 'Ubuntu', 'Cantarell', 'Noto Sans', 'sans-serif', 'Helvetica Neue', 'Arial', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+        'custom': ['Helvetica, Arial, sans-serif']
+
       }
     },
   },
